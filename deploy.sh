4d6db0ec685024b274d183ea6758462da4db3227
Pierre Le Corre <60463157+pierre-le-corre@users.noreply.github.com>
2020-11-27 12:14:58 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ DEPLOY_TAG=$1
 REPO=$2
 
 #Build and deploy inseefr/arc
-docker build -f app.Dockerfile -t inseefr/arc:$DEPLOY_TAG . 
+docker build -f app.Dockerfile -t $REPO/arc:$DEPLOY_TAG . 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push $REPO/arc:$DEPLOY_TAG
 
 #Build and deploy inseefr/arc-ws
-docker build -f app-ws.Dockerfile -t inseefr/arc-ws:$DEPLOY_TAG . 
+docker build -f app-ws.Dockerfile -t $REPO/arc-ws:$DEPLOY_TAG . 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push $REPO/arc-ws:$DEPLOY_TAG
